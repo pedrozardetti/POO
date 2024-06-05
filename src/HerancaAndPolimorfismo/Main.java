cfd152b47717b1dc33a05f5c7b3bb0068f2b3853
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-05 19:37:51 -0300

    feat: Menu com objeto paciente para treinar herança

---
+++ src/HerancaAndPolimorfismo/Main.java
@@ -0,0 +1,12 @@
+package HerancaAndPolimorfismo;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Pessoa p = new Pessoa("Maria", 34, "1234567");
+        p.imprimirInfo();
+
+    }
+
+}
