46c969cf7fcfe0eba18a77f56bbece501d92de77
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-05 23:58:54 -0300

    Classe filha da classe ItemBiblioteca

--- src/Ex3HerencaAndPolimorfismo/Livro.java
+++ src/Ex3HerencaAndPolimorfismo/Livro.java
@@ -1,2 +1,45 @@
-package Ex3HerencaAndPolimorfismo;public class Livro {
+package Ex3HerencaAndPolimorfismo;
+
+public class Livro extends ItemBiblioteca {
+
+    int numeroPaginas;
+    String autor;
+
+
+    public Livro(String titulo, int anoPublicacao, int numeroPaginas, String autor) {
+        super(titulo, anoPublicacao);
+        this.numeroPaginas = numeroPaginas;
+        this.autor = autor;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(int numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "numeroPaginas=" + numeroPaginas +
+                ", autor='" + autor + '\'' +
+                ", titulo='" + titulo + '\'' +
+                ", anoPublicacao=" + anoPublicacao +
+                '}';
+    }
+
+    @Override
+    public void imprimirInfo() {
+
+    }
 }
