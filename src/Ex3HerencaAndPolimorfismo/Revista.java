529e06b9bcc636e575d6f96604a2b02032956e8f
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-05 23:59:15 -0300

    Classe filha

--- src/Ex3HerencaAndPolimorfismo/Revista.java
+++ src/Ex3HerencaAndPolimorfismo/Revista.java
@@ -1,2 +1,47 @@
-package Ex3HerencaAndPolimorfismo;public class Revista {
+package Ex3HerencaAndPolimorfismo;
+
+public class Revista extends ItemBiblioteca {
+
+    int edicao;
+    String mesPublicacao;
+
+
+    public Revista(String titulo, int anoPublicacao, int edicao, String mesPublicacao) {
+        super(titulo, anoPublicacao);
+        this.edicao = edicao;
+        this.mesPublicacao = mesPublicacao;
+    }
+
+
+    public int getEdicao() {
+        return edicao;
+    }
+
+    public void setEdicao(int edicao) {
+        this.edicao = edicao;
+    }
+
+    public String getMesPublicacao() {
+        return mesPublicacao;
+    }
+
+    public void setMesPublicacao(String mesPublicacao) {
+        this.mesPublicacao = mesPublicacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Revista{" +
+                "edicao=" + edicao +
+                ", mesPublicacao='" + mesPublicacao + '\'' +
+                ", titulo='" + titulo + '\'' +
+                ", anoPublicacao=" + anoPublicacao +
+                '}';
+    }
+
+    public void imprimirInfo() {
+
+    }
+
+
 }
