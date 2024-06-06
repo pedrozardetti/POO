963dbf08222e1bd23242389b6dc5fefcf10b13f7
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-05 23:52:13 -0300

    Classe filha da classe Veiculo

--- src/Ex1Heranca/Carro.java
+++ src/Ex1Heranca/Carro.java
@@ -1,2 +1,18 @@
-package Ex1Heranca;public class Carro {
+package Ex1Heranca;
+
+public class Carro extends Veiculo {
+
+    int numeroDePortas;
+
+
+    public Carro(String marca, String modelo, int ano, int numeroDePortas) {
+        super(marca, modelo, ano);
+        this.numeroDePortas = numeroDePortas;
+    }
+
+    public void imprimirInfo() {
+        super.imprimirInfo();
+        System.out.println("O número de portas do carro é " + numeroDePortas);
+    }
+
 }
