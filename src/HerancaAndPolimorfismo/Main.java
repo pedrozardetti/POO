82c3f79686ea92fe8d699e96198642a330c92903
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-05 21:15:22 -0300

    feat: aplicação de um upcasting em Pessoa paciente e Pessoa enfermeiro (polimorfismo)

--- src/HerancaAndPolimorfismo/Main.java
+++ src/HerancaAndPolimorfismo/Main.java
@@ -4,9 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Paciente paciente = new Paciente("1234", "Sulamerica", "Pedro", 25, "19677696807");
+        System.out.println("Informações do paciente");
+
+        // Pessoa paciente é um upcasting
+        Pessoa paciente = new Paciente("1234", "Sulamerica", "Pedro", 25, "19677696807");
         paciente.imprimirInfo(); // As informações ao fazer paciente. são nativas da classe pai (Pessoa)
 
+        System.out.println("Informações do enfermeiro");
+
+        Pessoa enfermeiro = new Enfermeiro(12, "UTI", "123456", "Gabriel", 20, "12244435");
+        enfermeiro.imprimirInfo();
     }
 
 }
