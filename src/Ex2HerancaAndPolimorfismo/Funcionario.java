2a124e84868b6022519378674c14799806f9bdb9
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-05 23:53:52 -0300

    Classe pai que as outras classes herdam

--- src/Ex2HerancaAndPolimorfismo/Funcionario.java
+++ src/Ex2HerancaAndPolimorfismo/Funcionario.java
@@ -1,2 +1,38 @@
-package Ex2HerancaAndPolimorfismo;public class Funcionario {
+package Ex2HerancaAndPolimorfismo;
+
+public class Funcionario {
+
+    String nome;
+    double salario;
+
+
+    public Funcionario() {
+    }
+
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double calcularPagamento() {
+        return salario;
+    }
+
 }
