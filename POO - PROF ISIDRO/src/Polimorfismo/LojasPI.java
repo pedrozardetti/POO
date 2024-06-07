758b98fcc294b4444f108b19b3101f41a136701f
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-07 00:13:28 -0300

    Cria LojasPI para testar polimorfismo entre Cliente e ClienteVIP

---
+++ POO - PROF ISIDRO/src/Polimorfismo/LojasPI.java
@@ -0,0 +1,26 @@
+package Polimorfismo;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class LojasPI {
+
+    public static void main(String[] args) {
+        // A instância de c1 é Cliente e a instância de c2 é ClienteVIP
+        Cliente c1, c2; // c1 e c2 são referencias à cliente.
+
+        c1 = new Cliente("Isidro", "isidro@gmail.com", 500);
+        c2 = new ClienteVIP("Pedro", "pedrozardetti1@gmail.com", 500, 300);
+
+        System.out.println(c1);
+        System.out.println(c2);
+
+        if (c2.fazerCompra(650)) {
+            System.out.println("Conseguiu comprar");
+        } else {
+            System.out.println("Saldo insuficiente");
+        }
+        System.out.println(c2);
+
+    }
+
+}
