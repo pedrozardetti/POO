0010969237e6d7755fda695e205be7e200873a78
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-07 00:11:58 -0300

    feat: Implementação da classe Cliente com métodos de compra e quitação de dívida

---
+++ POO - PROF ISIDRO/src/Polimorfismo/Cliente.java
@@ -0,0 +1,43 @@
+package Polimorfismo;
+
+public class Cliente {
+    private String nome;
+    private String email;
+    private float credito;
+
+    public Cliente(String nome, String email, float credito) {
+        this.nome = nome;
+        this.email = email;
+        this.credito = credito;
+    }
+
+
+    public float getCredito() {
+        return credito;
+    }
+
+    public void setCredito(float credito) {
+        this.credito = credito;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente - " +
+                "Nome - '" + nome + '\'' +
+                ", Email - '" + email + '\'' +
+                ", R$ - " + credito +
+                '}';
+    }
+
+    public boolean fazerCompra(float valor) {
+        if (valor > credito) {
+            return false;
+        } else
+            credito -= valor;
+        return true;
+    }
+
+    public void quitarDivida(float valor) {
+        credito += valor;
+    }
+}
