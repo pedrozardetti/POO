27144dd0f0132cf0999cdb236426b0fc5f32e57b
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-10 20:44:58 -0300

    feat: classe que chama a interface e executa a conexão

--- InterfaceAndClasseAbsatrata/src/ExInterface/AppBancoDados.java
+++ InterfaceAndClasseAbsatrata/src/ExInterface/AppBancoDados.java
@@ -1,2 +1,15 @@
-package ExInterface;public class AppBancoDados {
+package ExInterface;
+
+public class AppBancoDados {
+
+    public static void main(String[] args) {
+        InterfaceBD x;
+        if (args[0].equals("mysql")) {
+            x = new DriverMySQL();
+        } else {
+            x = new DriverOracle();
+        }
+        x.executar("localhost");
+        x.executar("Select * from tabela");
+    }
 }
