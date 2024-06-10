2c3397edd81da62d71d9ff497bc11dd4b383124e
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-10 18:28:42 -0300

    feat: classe filha da classe QuestaoSimples

--- POO - PROF ISIDRO/src/Polimorfismo/Ex2/QuestaoComDica.java
+++ POO - PROF ISIDRO/src/Polimorfismo/Ex2/QuestaoComDica.java
@@ -1,2 +1,14 @@
-package Polimorfismo.Ex2;public class QuestaoComDica {
+package Polimorfismo.Ex2;
+
+public class QuestaoComDica extends QuestaoSimples {
+    private String dica;
+
+    public QuestaoComDica(String e, String r, String dica) {
+        super(e, r);
+        this.dica = dica;
+    }
+
+    public String aplicarQuestao() {
+        return "P:"+super.enunciado+"\n (dica)" + dica;
+    }
 }
