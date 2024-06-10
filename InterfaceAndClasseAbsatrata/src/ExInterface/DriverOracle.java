6d3e22f2b4f8a92a833b8d23449a9b63bf0ac495
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-10 20:46:18 -0300

    feat: classe que conecta ao Oracle

--- InterfaceAndClasseAbsatrata/src/ExInterface/DriverOracle.java
+++ InterfaceAndClasseAbsatrata/src/ExInterface/DriverOracle.java
@@ -1,2 +1,14 @@
-package ExInterface;public class DriverOracle {
+package ExInterface;
+
+import java.sql.SQLOutput;
+
+public class DriverOracle implements InterfaceBD {
+    @Override
+    public void conectar(String servidor) {
+        System.out.println("ORACLE> " + servidor + " Connected");
+    }
+
+    public void executar(String SQL) {
+        System.out.println("ORACLE> Executing " + SQL);
+    }
 }
