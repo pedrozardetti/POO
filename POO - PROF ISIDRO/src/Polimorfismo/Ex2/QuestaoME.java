b50e361ccf47b39b104fe99b18dde0d471948354
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-10 18:29:43 -0300

    feat: classe filha da classe QuestaoSimples

--- POO - PROF ISIDRO/src/Polimorfismo/Ex2/QuestaoME.java
+++ POO - PROF ISIDRO/src/Polimorfismo/Ex2/QuestaoME.java
@@ -1,2 +1,25 @@
-package Polimorfismo.Ex2;public class QuestaoME {
+package Polimorfismo.Ex2;
+
+public class QuestaoME extends QuestaoSimples {
+    private String alternativa1;
+    private String alternativa2;
+    private String alternativa3;
+    private String alternativa4;
+
+    public QuestaoME(String e, String r, String alternativa1, String alternativa2, String alternativa3, String alternativa4) {
+        super(e, r);
+        this.alternativa1 = alternativa1;
+        this.alternativa2 = alternativa2;
+        this.alternativa3 = alternativa3;
+        this.alternativa4 = alternativa4;
+    }
+
+    public String aplicarQuestao() {
+        return "P:" + super.enunciado + "\n" +
+                " a) " + alternativa1 + "\n" +
+                " b) " + alternativa2 + "\n" +
+                " c) " + alternativa3 + "\n" +
+                " d) " + alternativa4;
+
+    }
 }
