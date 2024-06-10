ca50e0539b3f42ac0ad15ba9e529263a5a1ac7bf
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-10 18:30:45 -0300

    feat: classe pai/superclasse

--- POO - PROF ISIDRO/src/Polimorfismo/Ex2/QuestaoSimples.java
+++ POO - PROF ISIDRO/src/Polimorfismo/Ex2/QuestaoSimples.java
@@ -1,2 +1,20 @@
-package Polimorfismo.Ex2;public class QuestaoSimples {
+package Polimorfismo.Ex2;
+
+public class QuestaoSimples {
+    protected String enunciado;
+    protected String resposta;
+
+    public QuestaoSimples(String e, String r) {
+        this.enunciado = e;
+        this.resposta = r;
+    }
+
+    public String aplicarQuestao() {
+        return "P: " + enunciado;
+    }
+
+    public boolean corrigir(String resposta) {
+        return this.resposta.equals(resposta);
+    }
+
 }
