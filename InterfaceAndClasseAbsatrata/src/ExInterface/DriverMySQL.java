9a462c112c79919b5efb905de014172d8351bdb0
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-10 20:45:35 -0300

    feat: classe que conecta ao mySQL

--- InterfaceAndClasseAbsatrata/src/ExInterface/DriverMySQL.java
+++ InterfaceAndClasseAbsatrata/src/ExInterface/DriverMySQL.java
@@ -1,2 +1,13 @@
-package ExInterface;public class DriverMySQL {
+package ExInterface;
+
+public class DriverMySQL implements InterfaceBD{
+    @Override
+    public void conectar(String servidor) {
+        System.out.println("MYSQL> Connecting on "+servidor);
+    }
+
+    @Override
+    public void executar(String SQL) {
+        System.out.println("MYSQL> " +SQL+ "executed");
+    }
 }
