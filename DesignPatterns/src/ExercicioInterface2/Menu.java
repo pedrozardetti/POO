e80318bb32019b8f9178b935244461de52b033cf
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-11 23:56:08 -0300

    feat: classe para interação com user

--- DesignPatterns/src/ExercicioInterface2/Menu.java
+++ DesignPatterns/src/ExercicioInterface2/Menu.java
@@ -1,2 +1,12 @@
-package ExercicioInterface2;public class Menu {
+package ExercicioInterface2;
+
+public class Menu {
+
+    public static void main(String[] args) {
+
+        Circulo circulo = new Circulo(12);
+        circulo.imprimeDados();
+
+
+    }
 }
