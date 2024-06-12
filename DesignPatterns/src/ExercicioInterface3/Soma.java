806c5e176b629b2925bd77a41b3204e788df113c
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-12 00:01:45 -0300

    feat: classe que realiza a soma

--- DesignPatterns/src/ExercicioInterface3/Soma.java
+++ DesignPatterns/src/ExercicioInterface3/Soma.java
@@ -1,2 +1,38 @@
-package ExercicioInterface3;public class Soma {
+package ExercicioInterface3;
+
+public class Soma implements OperacaoMatematica, Impressao {
+
+    private int a;
+    private int b;
+
+    public Soma(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    @Override
+    public int calcula(int a, int b) {
+        return a + b;
+    }
+
+    @Override
+    public void imprimeDados() {
+        System.out.println("O resultado da conta foi -> " + calcula(a, b));
+    }
 }
