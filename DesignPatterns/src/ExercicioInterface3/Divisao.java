82014efaf8bf3ff2d6535dd32514fcd63f37ad4e
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-11 23:57:56 -0300

    feat: classe que faz a divisao

--- DesignPatterns/src/ExercicioInterface3/Divisao.java
+++ DesignPatterns/src/ExercicioInterface3/Divisao.java
@@ -1,2 +1,38 @@
-package ExercicioInterface3;public class Divisao {
+package ExercicioInterface3;
+
+public class Divisao implements OperacaoMatematica, Impressao {
+
+    private int a;
+    private int b;
+
+    public Divisao(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    @Override
+    public int calcula(int a, int b) {
+        return a / b;
+    }
+
+    @Override
+    public void imprimeDados() {
+        System.out.println("O resultado da conta foi -> " + calcula(a, b));
+    }
 }
