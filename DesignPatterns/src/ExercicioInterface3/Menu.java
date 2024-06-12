8ce7dd2ec6c5e21bfc73f818f2f6db65a83b6857
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-11 23:58:49 -0300

    feat: classe que faz interação com user

--- DesignPatterns/src/ExercicioInterface3/Menu.java
+++ DesignPatterns/src/ExercicioInterface3/Menu.java
@@ -1,2 +1,11 @@
-package ExercicioInterface3;public class Menu {
+package ExercicioInterface3;
+
+public class Menu {
+
+    public static void main(String[] args) {
+
+        Multiplicacao mult = new Multiplicacao(10, 9);
+        mult.imprimeDados();
+
+    }
 }
