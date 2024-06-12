f957badbac4afac11e5684af47eef4b68cd4c78a
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-11 23:54:50 -0300

    feat: classe que implementa a FormaGeometrica

--- DesignPatterns/src/ExercicioInterface2/Circulo.java
+++ DesignPatterns/src/ExercicioInterface2/Circulo.java
@@ -1,2 +1,35 @@
-package ExercicioInterface2;public class Circulo {
+package ExercicioInterface2;
+
+public class Circulo implements FormaGeometrica, Impressao {
+
+    private double raio;
+
+    public Circulo(double raio) {
+        this.raio = raio;
+    }
+
+    public double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * (raio * raio);
+    }
+
+    @Override
+    public double comprimento() {
+        return 2 * Math.PI * raio;
+    }
+
+    @Override
+    public void imprimeDados() {
+        System.out.println("A área do círculo é -> " + area());
+        System.out.println("O comprimento do círculo é -> " + comprimento());
+    }
 }
+
