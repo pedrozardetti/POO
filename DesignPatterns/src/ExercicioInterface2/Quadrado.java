cb4438a2178bab52d06af5563c74117f6000eafd
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-11 23:54:17 -0300

    feat: classe que implementa a FormaGeometrica

--- DesignPatterns/src/ExercicioInterface2/Quadrado.java
+++ DesignPatterns/src/ExercicioInterface2/Quadrado.java
@@ -1,2 +1,33 @@
-package ExercicioInterface2;public class Quadrado {
+package ExercicioInterface2;
+
+import ExercicioInterface1.FormaGeometrica;
+
+public class Quadrado implements FormaGeometrica {
+
+    private double lado;
+
+    public Quadrado(double lado) {
+        this.lado = lado;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public double area() {
+        return lado * lado;
+    }
+
+    @Override
+    public double comprimento() {
+        return Math.sqrt(lado);
+
+    }
+
+
 }
