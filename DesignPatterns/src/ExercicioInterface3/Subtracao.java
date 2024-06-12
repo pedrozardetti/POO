e9fdbbe76bf42bdf4f0ad189959090ffa7e6128e
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-12 00:03:07 -0300

    feat: classe que realiza a subtração

--- DesignPatterns/src/ExercicioInterface3/Subtracao.java
+++ DesignPatterns/src/ExercicioInterface3/Subtracao.java
@@ -1,2 +1,38 @@
-package ExercicioInterface3;public class Subtracao {
+package ExercicioInterface3;
+
+public class Subtracao implements OperacaoMatematica, Impressao {
+
+    private int a;
+    private int b;
+
+    public Subtracao(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    @Override
+    public int calcula(int a, int b) {
+        return a - b;
+    }
+
+    @Override
+    public void imprimeDados() {
+        System.out.println("O resultado da conta foi -> " + calcula(a, b));
+    }
 }
