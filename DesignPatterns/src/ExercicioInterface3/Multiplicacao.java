6094e28d007156fcf45598e971576e0d045037ad
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-11 23:59:13 -0300

    feat: classe que faz multiplicacao

--- DesignPatterns/src/ExercicioInterface3/Multiplicacao.java
+++ DesignPatterns/src/ExercicioInterface3/Multiplicacao.java
@@ -1,2 +1,40 @@
-package ExercicioInterface3;public class Multiplicacao {
+package ExercicioInterface3;
+
+public class Multiplicacao implements OperacaoMatematica, Impressao {
+
+    private int a;
+    private int b;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public Multiplicacao(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public int calcula(int a, int b) {
+        return a * b;
+
+
+    }
+
+    @Override
+    public void imprimeDados() {
+        System.out.println("O resultado da conta foi -> " + calcula(a, b));
+    }
 }
