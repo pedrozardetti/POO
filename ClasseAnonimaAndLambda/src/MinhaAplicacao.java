f964e46efa2a2a8b62a15152f4cd7831814b3280
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-12 22:01:25 -0300

    feat: adiciona classe MinhaAplicacao com exemplos de uso de MeuObjeto e interfaces

--- ClasseAnonimaAndLambda/src/MinhaAplicacao.java
+++ ClasseAnonimaAndLambda/src/MinhaAplicacao.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class MinhaAplicacao {
+import javax.swing.*;
+import java.awt.event.*;
+
+public class MinhaAplicacao {
+    public static void main(String[] args) {
+        MeuObjeto o = new MeuObjeto();
+        o.metodoDoObjeto(new MinhaInterface() {
+            @Override
+            public void metodoDaInterface() {
+                System.out.println("Método da interface executando");
+
+            }
+        });
+        o.metodoDoObjeto(() -> System.out.println("Opa.. executando de novo"));
+
+        System.out.println("Fim do programa");
+
+        o.outroMetodoDoObjeto((e, f) -> {
+                    System.out.println("Valor do E = " + e);
+                    System.out.println("Valor do F = " + f);
+
+                },
+                Integer.parseInt(args[0]),
+                Float.parseFloat(args[1]));
+
+
+    }
 }
