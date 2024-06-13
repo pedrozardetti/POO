bd94862a1b1182c6b115cbe6ccf86996853e7eb2
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-12 22:00:46 -0300

    feat: adiciona classe MeuObjeto com métodos metodoDoObjeto e outroMetodoDoObjeto

--- ClasseAnonimaAndLambda/src/MeuObjeto.java
+++ ClasseAnonimaAndLambda/src/MeuObjeto.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class MeuObjeto {
+public class MeuObjeto {
+    public void metodoDoObjeto(MinhaInterface m) {
+        System.out.println("Estou dentro do método do objeto");
+        m.metodoDaInterface();
+    }
+
+    public void outroMetodoDoObjeto(InterfaceFuncional m2, int valor, float valor2) {
+        m2.executaMetodo(valor, valor2);
+    }
 }
